5cd6a8809abcf1f019716a4838c6c7767a0a8eca
mugunthanarjunan-glitch <mugunthanarjunan@gmail.com>
2026-03-18 15:38:17 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://workflow-saasp.onrender.com',
+  baseURL: 'https://workflow-saasp.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
